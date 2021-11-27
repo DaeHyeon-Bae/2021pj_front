2f2046c0ddf0771c3e2e46061e40c9ac10543c21
unknown <qptei12@gmail.com>
2021-11-27 16:43:11 +0900

    firstcommit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
 import './App.css';
+import Button from '@mui/material/Button';
+import {TextField} from '@mui/material';
+import {useState} from 'react';
 
 function App() {
+
+  const [userName, setUserName] = useState('');
+  const [userDesc, setUserDesc] = useState('');
+  const [userID, setUserID] = useState('');
+  const [userPW, setUserPW] = useState('');
+
+  const onClickUserRegist = async () =>{
+    const reg = await fetch('http://localhost:4000/users',{
+                  method: 'post',
+                  headers: {
+                    'Content-type': 'application/json'
+                  },
+                  body: JSON.stringify({
+                    "user_nm": userName,
+                    "user_desc": userDesc,
+                    "user_login_id": userID,
+                    "user_login_pw": userPW
+                  })
+    });
+    alert(userName+" "+userDesc+" "+userID);
+    return reg;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app-container'>
+      <div>
+        <TextField label="유저 이름"
+                   variant="standard"
+                   fullWidth
+                   value={userName}
+                   onChange={(e)=>setUserName(e.target.value)}
+                   />
+        <TextField label="유저 설명"
+                   variant="standard"
+                   fullWidth
+                   value={userDesc}
+                   onChange={(e)=>setUserDesc(e.target.value)}
+                   />
+        <TextField label="유저 아이디"
+                   variant="standard"
+                   fullWidth
+                   value={userID}
+                   onChange={(e)=>setUserID(e.target.value)}
+                   />
+        <TextField label="유저 패스워드"
+                   variant="standard"
+                   fullWidth
+                   value={userPW}
+                   onChange={(e)=>setUserPW(e.target.value)}
+                   />
+      </div>
+
+      <div>
+      <Button variant="contained"
+              fullWidth
+              onClick={()=>onClickUserRegist()}
+              >회원가입</Button>
+      </div>
+      
     </div>
   );
 }
